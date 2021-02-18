02b6faa6a84f03af4480c75b221672f77ad2c847
IrynaAndrieieva <74906927+IrynaAndrieieva@users.noreply.github.com>
2021-02-18 18:42:56 +0200

    Add files via upload
    Added new file

---
+++ Drugstore.Domain/Models/WarehouseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drugstore.Domain.Models
+{
+    public class WarehouseModel
+    {
+        public int Id { get; set; }
+        public string Conditions { get; set; } // условия хранения действующего вещества
+        public int NumberShelf { get; set; }
+
+        public int MedicinalSubstanceId { get; set; }
+        public MedicinalSubstanceModel MedicinalSubstance { get; set; }
+
+
+    }
+}
