91ebeaad0cf4736e334054d30f58686ebae09553
IrynaAndrieieva <74906927+IrynaAndrieieva@users.noreply.github.com>
2021-02-18 20:27:29 +0200

    Add files via upload
    Update-database

---
+++ Drugstore/Controllers/PrecursorController.cs
@@ -0,0 +1,26 @@
+﻿using Drugstore.Domain.Services.Interfaces;
+using Drugstore.Models.Basket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drugstore.Controllers
+{
+
+    public class PrecursorController
+    {
+        private readonly IPrecursorService _precursorService;
+
+        public PrecursorController(IPrecursorService precursorService)
+        {
+            _precursorService = precursorService;
+        }
+
+        public void AddPrecursorToDrug(AddPrecursorToDrugPostModel model)
+        {
+            _precursorService.AddPrecursorToDrug(model.DrugId, model.PrecursorId, model.CountInRecipe);
+        }
+    }
+}
---
+++ Drugstore/Models/Precursor/AddPrecursorToDrugPostModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drugstore.Models.Basket
+{
+    public class AddPrecursorToDrugPostModel
+    {
+        public int DrugId { get; set; }
+
+        public int PrecursorId { get; set; }
+
+        public int CountInRecipe { get; set; }
+    }
+}
